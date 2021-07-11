b409a061e861dbb9e0adbded3b4d8321d980797d
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-11 20:43:56 -0300

    Create the first components using props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import App2 from './components/App2';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App2 title ='Hello World'>
+      Today is gonna be a great day
+    </App2>
+
+    <App2 title ='Hello Again'>
+      Tomorrow is gonna be a better day
+    </App2>
   </React.StrictMode>,
   document.getElementById('root')
 );
