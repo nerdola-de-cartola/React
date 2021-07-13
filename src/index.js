c8e25140d9f3d7cbdd1de432c9ff5119382a5b17
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-13 13:51:57 -0300

    Component life cicle

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './components/App';
 import App2 from './components/App2';
 import App3 from './components/App3';
 import App4 from './components/App4';
+import App5 from './components/App5';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -22,8 +23,11 @@ ReactDOM.render(
     {/* <App3>
     </App3> */}
 
-    <App4>
-    </App4>
+    {/* <App4>
+    </App4> */}
+
+    <App5>
+    </App5>
   </React.StrictMode>,
   document.getElementById('root')
 );
