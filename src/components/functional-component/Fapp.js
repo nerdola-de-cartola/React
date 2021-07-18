2eb192e88f30af19c3c6a421b7be87af6bef3910
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-17 23:34:59 -0300

    Hook Efects

--- src/components/functional-component/Fapp.js
+++ src/components/functional-component/Fapp.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Fapp() {
-    const [name, setName] = useState('')
+    const [name, setName] = useState(undefined)
+
+    useEffect(
+        () => {
+            if (name === undefined) {
+                setName(sessionStorage.getItem('name') || '')
+            } else {
+                sessionStorage.setItem('name', name)
+            }
+        },
+        [name]
+    )
 
     return (
         <>
-            Name: <input type='text' value={name} onChange={(event) => setName(event.target.value)}/>
-            <br/>
+            Name: <input type='text' value={name} onChange={(event) => setName(event.target.value)} />
+            <br />
             Olá {name}
         </>
     )
